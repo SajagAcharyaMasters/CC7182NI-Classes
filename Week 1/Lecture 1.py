82e0e3e2f1f1bd10aaa448068c9b2426e216d1a1
Sajag <sajag.acharya@islingtoncollege.edu.np>
2025-06-16 08:20:17 +0545

    - Week 1 Complete

--- Week 1/Lecture 1.py
+++ Week 1/Lecture 1.py
@@ -7,6 +7,25 @@ def charInfo():
     print('bin:', bin(ascii), format(ascii, 'b'))
     print('hex:', hex(ascii), format(ascii, 'x'))
 
+def whileLooper():
+    acc = inp = 0
+    count = 0
+    minm = maxm = None # Using list, min(list) and max(list) works
+    while inp != 'q':
+        inp = input('Enter number (q to quit): ')
+        try:
+            x = int(inp)
+            acc += x
+            count += 1
+            if  minm is None or x < minm: minm = x
+            elif maxm is None or x > maxm: maxm = x
+            print('sum:', acc)
+        except Exception as e:
+            # continue
+            print(f'Invalid input: {inp}' if inp != 'q' else f'\nFinal sum: {acc}')
+    print('Average:', acc / count)
+    print('Min:', minm)
+    print('Max:', maxm)
 
 if __name__ == '__main__':
-    charInfo()
+    whileLooper()
