4568a643078a1383db1dad3632a2908d3dd0bfea
Sajag <sajag.acharya@islingtoncollege.edu.np>
2025-06-29 08:25:18 +0545

    - Updated longest word to ignore case

--- Week 4/W4 - longest word.py
+++ Week 4/W4 - longest word.py
@@ -1,14 +1,14 @@
 # input_word = 'typhoon'
 input_word = input('Enter a word: ')
-input_set = set(input_word)
+input_set = set(input_word.lower())
 if __name__ == '__main__':
     fileName = '../Week 3/words.txt'
     with open(fileName, encoding='utf8') as file:
         matches = []
         for line in file:
-            word = line.strip()
+            word = line.strip().lower()
             if set(word) == input_set:
                 matches.append(word)
-    matches = sorted(matches, key=lambda w: len(w), reverse=True)
+    matches = sorted(matches, key=len, reverse=True)
     print(*matches)
     print('Longest word:', matches[0] if len(matches) > 0 else None)
